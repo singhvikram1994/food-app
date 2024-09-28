0e5cf8b893b3df2ba24cf49d9916bc8ba6e2690e
Vikram Singh <singh.vikram1994@gmail.com>
2024-09-28 16:07:43 -0400

    Add files via upload

---
+++ dist/js/0.bundle.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (23);\n\n//# sourceURL=webpack:///./src/js/main.js?");
+
+/***/ })
+
+}]);
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+
+import age from './main'
+
+console.log(`MY Age is :${age}`)
